d10618b128dfeb1119f579ddd8c1e60daa087b07
vaidya0303 <vaidya0304@gmail.com>
2022-10-19 18:21:23 +0530

    create binary tree from figure

--- MainBST.java
+++ MainBST.java
@@ -1,17 +1,20 @@
 package BTS;
 
 /**
- *  Ability to create a BST by adding 56 and then adding 30 & 70
- *   Use INode to create My Binary Node
- *   Note the key has to extend comparable to compare and determine left or right node
- *   First add 56 as root node so 30 will be added to left and 70 to right
+ * 1. Ability to create a BST by adding 56 and then adding 30 & 70
+ *    Use INode to create My Binary Node
+ *    Note the key has to extend comparable to compare and determine left or right node
+ *    First add 56 as root node so 30 will be added to left and 70 to right
+ *
+ * 2. Ability to create the binary tree shown in the figure
+ *    Check if all are added with using size method in Binary Tree
  *
  */
 
 /*
  * main class name as MainBst
  */
-public class MainBst {
+public class MainBST {
 
     /*
      * main method all program execute in main method
@@ -52,15 +55,19 @@ public class MainBst {
         // 30->56->70->80
         root = bst.insert(root, 70);
 
-        /*
-         * printing the inserting data
-         *
-         * 56,30,70
-         */
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
+
         bst.print(root);
-
-
-
+        
     }
 
 }
